9d6ab21162e43e81e20af2947974eb9f682af13c
mbach <mbach@fedora31.localdomain>
2019-12-06 10:19:31 -0500

    add json endpoint

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,29 @@
 from flask import Flask
+from flask import jsonify
+
 application = Flask(__name__)
+
+
+json_data = {
+    "username": "admin",
+    "email": "admin@localhost",
+    "id": 42
+}
 
 @application.route("/")
 def hello():
     return "Hello World!"
+
+
+@application.route("/json-test")
+def json_test:
+    return jsonify(
+        {
+            "username": "admin",
+            "email": "admin@localhost",
+            "id": 42
+        }
+    )
 
 if __name__ == "__main__":
     application.run()
